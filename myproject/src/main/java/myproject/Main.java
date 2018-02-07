f59a69afe83804582729c4741d0a23dd98a200c5
Diego <diego.campanella93@gmail.com>
2018-02-07 13:11:30 +0100

    added junit add

--- myproject/src/main/java/myproject/Main.java
+++ myproject/src/main/java/myproject/Main.java
@@ -1,5 +1,9 @@
 package myproject;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,6 +11,12 @@ public class Main {
 		System.out.println("Prova");
 		System.out.println("Fixed Issue #1");
 
+	}
+	
+	@Test
+	public void myTest() {
+		String message = "Test";
+		assertNotNull(message);
 	}
 
 }
